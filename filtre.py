ad486dc288e2cd2ccdf6a833f4b337bf2037a741
Nicolas-77950 <nicolas.cheel@edu.esiee-it.fr>
2024-12-11 13:45:27 +0100

    add function rotated

--- filtre.py
+++ filtre.py
@@ -3,6 +3,7 @@ from PIL import ImageFilter
 from logger import *
 import os
 from Importation import *
+from PIL import ImageFont, ImageDraw
 
 def apply_blur_filter(images, effect , n):
     """
@@ -34,7 +35,40 @@ output_folder = "img/modified"  # Dossier pour sauvegarder les images floutées
 images_list = import_images_from_folder(input_folder)
 print(images_list[0][1])
 
-apply_blur_filter(images, output_folder)
+#apply_blur_filter(images, output_folder)
 
 
+def image_rotated(images, output_folder, angle):
+    '''
+    Pivote les images selon l'angle saisi et les sauvegarde.
 
+    Args:
+        images (list): Liste de tuples (nom_fichier, PIL.Image.Image) représentant les images à pivoter.
+        output_folder (str): Chemin vers le dossier où sauvegarder les images pivoters.
+        angle (float): Angle de rotation en degrés.
+    '''
+    try:
+        # Crée le dossier de sortie s'il n'existe pas
+        os.makedirs(output_folder, exist_ok=True)
+        for img in images:
+            # Applique la rotation
+            rotated_img = img[1].rotate(angle, expand=True)
+            # Génère le chemin du fichier de sortie
+            output_path = os.path.join(output_folder, f"rotated_{img[0]}")
+            # Sauvegarde l'image pivoter
+            rotated_img.save(output_path)
+            print(f"Image pivoter sauvegardée : {output_path}")
+
+    except Exception as e:
+        print(f"Erreur lors de la rotation des images : {e}")
+
+# Chemins des dossiers
+input_folder = "img/default"  # Dossier contenant les images
+output_folder_rotate = "img/modified/rotate"  # Dossier pour sauvegarder les images pivoters
+
+# Importation des images
+images_list = import_images_from_folder(input_folder)
+
+# Application de la rotation
+angle = 40
+image_rotated(images_list, output_folder_rotate, angle)
